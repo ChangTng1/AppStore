ce83f9f1a260ccd8592eef519fca952a4c86cb3f
ChangTng1 <101157125+ChangTng1@users.noreply.github.com>
2022-03-20 16:14:09 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -129,12 +129,12 @@ def rental(request, Listingid):
                 last_entry = cursor.fetchone()
                 if (datetime.strptime(request.POST['Start_day'], '%Y-%m-%d').date() == listing[4]):
                     cursor.execute("INSERT INTO GPU_Listing VALUES(%s, %s,%s,%s,%s,%s,%s)", [last_entry[0] + 1 ,listing[1], listing[2], listing[3], 
-                                                                                         request.POST['End_day'] + 1, listing[5], listing[6]])
+                                                                                         datetime.strptime(request.POST['End_day'], '%Y-%m-%d').date() + 1, listing[5], listing[6]])
                 if (datetime.strptime(request.POST['Start_day'], '%Y-%m-%d').date() > listing[4]):
                     cursor.execute("INSERT INTO GPU_Listing VALUES(%s, %s,%s,%s,%s,%s,%s)", [last_entry[0] + 1 ,listing[1], listing[2], listing[3], 
-                                                                                         listing[4], request.POST['Start_day'] - 1, listing[6]])
+                                                                                         listing[4], datetime.strptime(request.POST['Start_day'], '%Y-%m-%d').date() - 1, listing[6]])
                     cursor.execute("INSERT INTO GPU_Listing VALUES(%s, %s,%s,%s,%s,%s,%s)", [last_entry[0] + 2 ,listing[1], listing[2], listing[3], 
-                                                                                         request.POST['End_day'] + 1, listing[5], listing[6]])
+                                                                                         datetime.strptime(request.POST['End_day'], '%Y-%m-%d').date() + 1, listing[5], listing[6]])
                 return redirect('index')    
             else:
                 status = 'Invalid Rental Dates'
